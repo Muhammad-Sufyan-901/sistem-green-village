47add60c203e229b9f1083b34230c512c3dd3392
Muhammad-Sufyan-901 <muhammadsufyann09@gmail.com>
2025-01-26 09:07:50 +0800

    feat : add javascript to add value when trip is selected

--- resources/views/backend/app.blade.php
+++ resources/views/backend/app.blade.php
@@ -1,74 +1,83 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>{{ config('app.name') }} | Dashboard</title>
-  <meta name="description" content="{{ config('app.name') }} | Dashboard" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ config('app.name') }} | Dashboard</title>
+    <meta name="description" content="{{ config('app.name') }} | Dashboard" />
 
-  <!-- Favicon -->
-  <link rel="icon" type="image/x-icon" href="{{ asset('img/logo.png') }}" />
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="{{ asset('img/logo.png') }}" />
 
-  <!-- Fonts -->
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+        rel="stylesheet" />
 
-  <!-- Icons. Uncomment required icon fonts -->
-  <link rel="stylesheet" href="{{ asset('vendors/backend/fonts/boxicons.css') }}" />
+    <!-- Icons. Uncomment required icon fonts -->
+    <link rel="stylesheet" href="{{ asset('vendors/backend/fonts/boxicons.css') }}" />
 
-  <!-- Core CSS -->
-  <link rel="stylesheet" href="{{ asset('vendors/backend/css/core.css') }}" class="template-customizer-core-css" />
-  <link rel="stylesheet" href="{{ asset('vendors/backend/css/theme-default.css') }}" class="template-customizer-theme-css" />
-  <link rel="stylesheet" href="{{ asset('css/backend/demo.css') }}" />
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="{{ asset('vendors/backend/css/core.css') }}" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="{{ asset('vendors/backend/css/theme-default.css') }}"
+        class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="{{ asset('css/backend/demo.css') }}" />
 
-  <!-- Page CSS -->
-  <!-- Page -->
-  <link rel="stylesheet" href="{{ asset('vendors/backend/css/pages/page-auth.css') }}" />
-  <!-- Helpers -->
-  <script src="{{ asset('vendors/backend/js/helpers.js') }}"></script>
-  <script src="{{ asset('js/backend/config.js') }}"></script>
+    <!-- Page CSS -->
+    <!-- Page -->
+    <link rel="stylesheet" href="{{ asset('vendors/backend/css/pages/page-auth.css') }}" />
+    <!-- Helpers -->
+    <script src="{{ asset('vendors/backend/js/helpers.js') }}"></script>
+    <script src="{{ asset('js/backend/config.js') }}"></script>
 </head>
+
 <body>
-  <div class="layout-wrapper layout-content-navbar">
-    <div class="layout-container">
-      {{-- Sidemenu --}}
-      @include('backend.partials.sidemenu')
+    <div class="layout-wrapper layout-content-navbar">
+        <div class="layout-container">
+            {{-- Sidemenu --}}
+            @include('backend.partials.sidemenu')
 
-      {{-- Layout Container --}}
-      <div class="layout-page">
-        {{-- Navbar --}}
-        @include('backend.partials.navbar')
+            {{-- Layout Container --}}
+            <div class="layout-page">
+                {{-- Navbar --}}
+                @include('backend.partials.navbar')
 
-        {{-- Content --}}
-        <div class="content-wrapper">
-          @yield('content')
+                {{-- Content --}}
+                <div class="content-wrapper">
+                    @yield('content')
+                </div>
+
+                {{-- Footer --}}
+                @include('backend.partials.footer')
+
+                <div class="content-backdrop fade"></div>
+            </div>
         </div>
 
-        {{-- Footer --}}
-        @include('backend.partials.footer')
-
-        <div class="content-backdrop fade"></div>
-      </div>
+        <!-- Overlay -->
+        <div class="layout-overlay layout-menu-toggle"></div>
     </div>
 
-    <!-- Overlay -->
-    <div class="layout-overlay layout-menu-toggle"></div>
-  </div>
-  
-  <!-- Core JS -->
-  <!-- build:js assets/vendor/js/core.js -->
-  <script src="{{ asset('vendors/backend/libs/jquery/jquery.js') }}"></script>
-  <script src="{{ asset('vendors/backend/libs/popper/popper.js') }}"></script>
-  <script src="{{ asset('vendors/backend/js/bootstrap.js') }}"></script>
-  <script src="{{ asset('vendors/backend/js/menu.js') }}"></script>
+    <!-- Core JS -->
+    <!-- build:js assets/vendor/js/core.js -->
+    <script src="{{ asset('vendors/backend/libs/jquery/jquery.js') }}"></script>
+    <script src="{{ asset('vendors/backend/libs/popper/popper.js') }}"></script>
+    <script src="{{ asset('vendors/backend/js/bootstrap.js') }}"></script>
+    <script src="{{ asset('vendors/backend/js/menu.js') }}"></script>
 
-  <!-- Main JS -->
-  <script src="{{ asset('vendors/jquery-validation/jquery.validate.min.js') }}"></script>
-  <script src="{{ asset('js/backend/main.js') }}"></script>
+    <!-- Main JS -->
+    <script src="{{ asset('vendors/jquery-validation/jquery.validate.min.js') }}"></script>
+    <script src="{{ asset('js/backend/main.js') }}"></script>
 
-  <script>upload_transport_price_list();</script>
-  @yield('script')
+    <script>
+        upload_transport_price_list();
+    </script>
+    @yield('script')
+    @stack('js')
 </body>
+
 </html>
--- resources/views/backend/payment_request/create.blade.php
+++ resources/views/backend/payment_request/create.blade.php
@@ -1,87 +1,134 @@
 @extends('backend.app')
 
 @section('content')
-  <div class="container-xxl flex-grow-1 container-p-y">
-    {{-- Breadcrumb --}}
-    <nav aria-label="breadcrumb">
-      <ol class="breadcrumb">
-        <li class="breadcrumb-item">
-          <a href="{{ Route('backend.dash.home') }}">Home</a>
-        </li>
-        <li class="breadcrumb-item">
-          <a href="{{ Route('backend.payment-request.index') }}">Payment Requests</a>
-        </li>
-        <li class="breadcrumb-item active">Create</li>
-      </ol>
-    </nav>
+    <div class="container-xxl flex-grow-1 container-p-y">
+        {{-- Breadcrumb --}}
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item">
+                    <a href="{{ Route('backend.dash.home') }}">Home</a>
+                </li>
+                <li class="breadcrumb-item">
+                    <a href="{{ Route('backend.payment-request.index') }}">Payment Requests</a>
+                </li>
+                <li class="breadcrumb-item active">Create</li>
+            </ol>
+        </nav>
 
-    {{-- Form --}}
-    <div class="alert-message-box"></div>
-    <h5 class="mt-4 mb-4">Create Payment Request</h5>
-    <form action="{{ Route('backend.payment-request.store') }}" method="POST" id="addForm" enctype="multipart/form-data">
-      @csrf
-      <div class="card mb-4">
-        <div class="card-body">
-          <div class="row">
-            <div class="col-md-12 mb-3"> 
-    <label class="form-label">Select Trip <span class="text-danger">*</span></label> 
-    <select name="select_trip" class="form-select" base_error="Err_select_trip" id="trip-select"> 
-        <option value="">-- Please Select --</option> 
-        @foreach ($trips as $key => $item) 
-            @php 
-                $totalAlreadyRequest = \App\Models\PaymentRequest::where('trip_id', $item->trip_id)
-                    ->where('status', '!=', 'cancel')
-                    ->count(); 
-            @endphp 
-            @if ($totalAlreadyRequest == 0) 
-                <option value="{{ $item->trip_id }}" 
-                    data-trip-type="{{ $item->re_type_trip->name ?? '' }}"
-                    data-amount="{{ $item->re_type_trip->name == 'full day' ? 500000 : ($item->re_type_trip->name == 'half day' ? 250000 : '') }}"> 
-                    {{ $item->name }} {{ isset($item->re_type_trip) ? ' - '.$item->re_type_trip->name : '' }} 
-                </option> 
-            @endif 
-        @endforeach 
-    </select>
-    <div id="Err_select_trip" class="text-danger"></div> 
-</div>
-            <div class="col-md-12 mb-3">
-              <label class="form-label">Upload Proofs <span class="text-danger">*</span></label>
-              <input class="form-control" type="file" name="proofs[]" base_error="Err_proofs" multiple="multiple" accept="image/png, image/jpg, image/jpeg">
-              <div id="Err_proofs" class="text-danger"></div>
+        {{-- Form --}}
+        <div class="alert-message-box"></div>
+        <h5 class="mt-4 mb-4">Create Payment Request</h5>
+        <form action="{{ Route('backend.payment-request.store') }}" method="POST" id="addForm"
+            enctype="multipart/form-data">
+            @csrf
+            <div class="card mb-4">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-12 mb-3">
+                            <label class="form-label">Select Trip <span class="text-danger">*</span></label>
+                            <select name="select_trip" class="form-select" base_error="Err_select_trip" id="trip-select">
+                                <option value="">-- Please Select --</option>
+                                @foreach ($trips as $key => $item)
+                                    @php
+                                        $totalAlreadyRequest = \App\Models\PaymentRequest::where(
+                                            'trip_id',
+                                            $item->trip_id,
+                                        )
+                                            ->where('status', '!=', 'cancel')
+                                            ->count();
+                                    @endphp
+                                    @if ($totalAlreadyRequest == 0)
+                                        <option value="{{ $item->trip_id }}"
+                                            data-trip-type="{{ $item->re_type_trip->name ?? '' }}"
+                                            data-amount="{{ $item->re_type_trip->name == 'Full Day' ? 500000 : ($item->re_type_trip->name == 'Half Day' ? 250000 : '') }}"
+                                            data-is-tour={{ $item->re_type_trip->name == 'Tour' ? 1 : 0 }}>
+                                            {{ $item->name }}
+                                            {{ isset($item->re_type_trip) ? ' - ' . $item->re_type_trip->name : '' }}
+                                        </option>
+                                    @endif
+                                @endforeach
+                            </select>
+                            <div id="Err_select_trip" class="text-danger"></div>
+                        </div>
+                        <div class="col-md-12 mb-3">
+                            <label class="form-label">Upload Proofs <span class="text-danger">*</span></label>
+                            <input class="form-control" type="file" name="proofs[]" base_error="Err_proofs"
+                                multiple="multiple" accept="image/png, image/jpg, image/jpeg">
+                            <div id="Err_proofs" class="text-danger"></div>
+                        </div>
+                        <div class="col-md-12 mb-3">
+                            <label class="form-label">Description</label>
+                            <textarea name="description" class="form-control" cols="30" rows="5" base_error="Err_description"></textarea>
+                            <div id="Err_description" class="text-danger"></div>
+                        </div>
+                        <div class="col-md-12 mb-3" id="additional-charges" style="display: none">
+                            <label class="form-label"> Additional Charges <span class="text-danger">*</span></label>
+                            <input class="form-control kdef-rate-format" type="text" name="additional_charges"
+                                base_error="Err_additional_charges">
+                            <div id="Err_additional_charges" class="text-danger"></div>
+                        </div>
+                        <div class="col-md-12 mb-3">
+                            <label class="form-label">Total Rates Amount <span class="text-danger">*</span></label>
+                            <input class="form-control kdef-rate-format" type="text" name="total_rates_amount"
+                                base_error="Err_total_rates_amount">
+                            <div id="Err_total_rates_amount" class="text-danger"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <button class="btn rounded-pill btn-primary btn-submit">Submit</button>
+                </div>
             </div>
-            <div class="col-md-12 mb-3">
-              <label class="form-label">Description</label>
-              <textarea name="description" class="form-control" cols="30" rows="5" base_error="Err_description"></textarea>
-              <div id="Err_description" class="text-danger"></div>
-            </div>
-            <div class="col-md-12 mb-3">
-              <label class="form-label">Total Rates Amount <span class="text-danger">*</span></label>
-              <input class="form-control kdef-rate-format" type="text" name="total_rates_amount" base_error="Err_total_rates_amount">
-              <div id="Err_total_rates_amount" class="text-danger"></div>
-            </div>
-          </div>
-        </div>
-        <div class="card-footer">
-          <button class="btn rounded-pill btn-primary btn-submit">Submit</button>
-        </div>
-      </div>
-    </form>
-  </div>
+        </form>
+    </div>
 @endsection
 
-@section('script')
+{{-- @section('script')
+    <script>
+        console.log('payment_request_store');
 
-<script>
-  function payment_request_store() {
-    console.log('payment_request_store');
-}
-</script>
-  <script src="{{ asset('js/backend/functions/payment_request.js') }}"></script>
-  <script defer="defer">
-    window.addEventListener('DOMContentLoaded', function() {
-      payment_request_store();
-    });
-  </script>
-@endsection
+        function payment_request_store() {
+            console.log('payment_request_store');
+        }
+    </script>
+    <script src="{{ asset('js/backend/functions/payment_request.js') }}"></script>
+    <script defer="defer">
+        window.addEventListener('DOMContentLoaded', function() {
+            payment_request_store();
+        });
+    </script>
+@endsection --}}
 
+@push('js')
+    <script src="{{ asset('js/backend/functions/payment_request.js') }}"></script>
+@endpush
 
+@push('js')
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const tripSelect = document.getElementById('trip-select');
+            const totalRatesAmountInput = document.querySelector('input[name="total_rates_amount"]');
+            const additionalChargesInputField = document.querySelector('#additional-charges');
+
+            if (tripSelect && totalRatesAmountInput) {
+                tripSelect.addEventListener('change', function() {
+                    const selectedOption = tripSelect.options[tripSelect.selectedIndex];
+                    const amount = selectedOption.getAttribute('data-amount');
+                    const isTour = selectedOption.getAttribute('data-is-tour');
+
+                    if (amount) {
+                        totalRatesAmountInput.value = amount;
+                    } else {
+                        totalRatesAmountInput.value = '';
+                    }
+
+                    if (isTour == 1) {
+                        additionalChargesInputField.style.display = 'block';
+                    } else {
+                        additionalChargesInputField.style.display = 'none';
+                    }
+                });
+            }
+        });
+    </script>
+@endpush
